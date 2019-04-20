790574da81fe26642482400b63192ebedf5b4125
Judson Stevens <Judson.Stevens@gmail.com>
2019-04-20 10:28:29 -0600

    Finished up the confirmation email and confirm_email route

--- app/controllers/user_confirmation_controller.rb
+++ app/controllers/user_confirmation_controller.rb
@@ -1,5 +1,10 @@
 class UserConfirmationController < ApplicationController
   def confirm_email
-    
+    user = User.find_by(confirmation_code: params[:id])
+    if user
+      user.activate_user
+      redirect_to 'https://medapp.com/successful_activation'
+    else
+      redirect_to 'https://medapp.com/unsuccessful_activation'
   end
 end
