39dd32daad24e38b7eaf69f7ffc7e4e4bfaeccfe
Judson Stevens <Judson.Stevens@gmail.com>
2019-04-20 19:50:33 -0600

    Started the process CSV class to take in CSV's for patient information processing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,7 +10,7 @@ class UserController < ApplicationController
   def update
     user = User.find_by(email: params[:email])
 
-    if params[:password] && @current_user.password == params[:old_password]
+    if params[:password] && params[:old_password] && @current_user.password == params[:old_password]
       render json: {message: "#{@current_user.email}'s password has been successfully changed."}, status: :ok if @current_user.reset_password(params[:password])
     else
       render json: {message: "#{@current_user.email}'s password was not changed.", errors: @current_user.errors.full_messages}, status: :unprocessable_entity
