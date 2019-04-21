4f59c253e7986419dbe556c97707a649127bf315
Judson Stevens <Judson.Stevens@gmail.com>
2019-04-20 20:09:25 -0600

    Switched to using the upload process from React front end so the file doesn't have to be stored

--- lib/process_patient_csv.rb
+++ lib/process_patient_csv.rb
@@ -1,16 +1,13 @@
 require 'csv'
-require 'aws-sdk-s3'
 
 class ProcessPatientCSV
-  def initialize(region, bucket, csv_path)
-    s3 = Aws::S3::Resource.net(region: region)
-    s3_object = s3.bucket(bucket).object(csv_path)
-    @csv = s3_object.get.body
+  def initialize(temp_csv)
+    @csv = temp_csv
   end
 
   def add_patient_information
     CSV.parse(@csv, headers: true).each do
-      
+
     end
   end
 end
