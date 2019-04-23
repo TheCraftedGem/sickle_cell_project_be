2525b30eb9875183645dff55324ca29b0364b9b7
Emilio <thecraftedgem@tuta.io>
2019-04-23 10:20:40 -0600

    Add Patient Model And Test

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -6,5 +6,7 @@ require 'rails_helper'
       it {should validate_presence_of(:first_name)}
       it {should validate_presence_of(:last_name)}
       it {should validate_presence_of(:password_digest)}
+      it {should validate_presence_of(:role)}
+      it {should validate_presence_of(:status)}
     end
   end
