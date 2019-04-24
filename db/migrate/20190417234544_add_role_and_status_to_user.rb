6ecc0c92fb582b2d21193099cc7535ff8d52baf3
Emilio <thecraftedgem@tuta.io>
2019-04-24 12:08:12 -0600

    Adds Default Check Test To User Spec

--- db/migrate/20190417234544_add_role_and_status_to_user.rb
+++ db/migrate/20190417234544_add_role_and_status_to_user.rb
@@ -1,6 +1,6 @@
 class AddRoleAndStatusToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :role, :integer
-    add_column :users, :status, :integer
+    add_column :users, :role, :integer, default: 0
+    add_column :users, :status, :integer, default: 0
   end
 end
