1a329cc2c400bebe814f8ea307db90019ec681ce
Emilio <thecraftedgem@tuta.io>
2019-09-02 15:19:10 -0600

    fixes email confirmation request/response

--- spec/controllers/user_confirmations_controller_spec.rb
+++ spec/controllers/user_confirmations_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'jwt'
 
-RSpec.describe Api::V1::UserConfirmationsController, type: :api do
+RSpec.describe UserConfirmationsController, type: :api do
   context 'When a user clicks the confirmation link' do
     
   end
