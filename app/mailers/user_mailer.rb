187b8e895d8e6445a3f47da33b11f7a79fc7dcd6
Judson Stevens <Judson.Stevens@gmail.com>
2019-09-05 18:16:01 -0600

    Added email functionality, should have been in another branch

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ApplicationMailer
-  default from: 'NoReply@medapp.com'
+  default from: 'NoReply@providerminder.com'
 
   def confirmation_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Confirmation Email for MedApp')
+    @qr = RQRCode::QRCode.new(@user.provisioning_uri("Provider Minder"), size: 8, level: :h)
+    mail(to: @user.email, subject: 'Confirmation Email for Provider Minder')
   end
 
   def self.password_reset_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Reset Your Password for MedApp')
+    mail(to: @user.email, subject: 'Reset Your Password for Provider Minder')
   end
 end
