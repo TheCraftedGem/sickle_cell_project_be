2ae2681aa4890a79d7f1db62c6ff7b334d0af39f
Judson Stevens <Judson.Stevens@gmail.com>
2019-09-05 21:58:43 -0600

    Removed pry from user controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::AuthenticationController < ApplicationController
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
     if command.success?
-
       render json: {auth_token: command.result[0]}
     else
       render json: {error: command.errors}, status: :unauthorized
